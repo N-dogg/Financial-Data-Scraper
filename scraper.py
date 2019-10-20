08237abedd5bc83697b2ac51b3953709c9ebbb44
N-dogg <43980002+N-dogg@users.noreply.github.com>
2019-10-21 10:13:35 +1100

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,48 @@
+import urllib
+import pandas as pd
+import win32com.client as win32
+from openpyxl import load_workbook
+import os
+
+url = 'insert_url_here.com'
+filename = 'test_{0}.xls'
+path = r'file_path'
+
+asx = pd.read_csv('codes.csv')
+
+
+for i in asx['ASX Code']:
+    web_path = url.format(i)
+    try:
+        urllib.request.urlretrieve(path, filename.format(i))
+    except:
+        pass
+
+for i in asx['ASX Code']:
+    fname = path.format(i)
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    try:
+        wb = excel.Workbooks.Open(fname)
+        wb.SaveAs(fname+"x", FileFormat = 51)
+        wb.Close()
+        excel.Application.Quit()
+        os.remove(filename.format(i))
+    except:
+        pass
+
+sheet = 'Sheet{0}'
+filename_x = 'test_{0}.xlsx'
+
+book = load_workbook('Book1.xlsx')
+writer = pd.ExcelWriter('Book1.xlsx', engine='openpyxl')
+writer.book = book
+
+for i in asx['ASX Code']:
+    file = filename_x.format(i)
+    try:
+        df = pd.read_excel(file)
+        sh = sheet.format(i)
+        df.to_excel(writer, sh)
+        writer.save()
+    except:
+        pass

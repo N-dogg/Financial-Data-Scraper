4f012de1ac3ee807bb3d1f8cff6635a1db45c73f
N-dogg <43980002+N-dogg@users.noreply.github.com>
2019-10-23 18:25:24 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,13 +4,14 @@ import win32com.client as win32
 from openpyxl import load_workbook
 import os
 
+#url, filename & path variables
 url = 'insert_url_here.com'
 filename = 'test_{0}.xls'
 path = r'file_path'
 
 asx = pd.read_csv('codes.csv')
 
-
+#interate through each asx code and download .xls file
 for i in asx['ASX Code']:
     web_path = url.format(i)
     try:
@@ -18,6 +19,7 @@ for i in asx['ASX Code']:
     except:
         pass
 
+#convert each file to .xlsx and remove .xls file    
 for i in asx['ASX Code']:
     fname = path.format(i)
     excel = win32.gencache.EnsureDispatch('Excel.Application')
@@ -29,14 +31,16 @@ for i in asx['ASX Code']:
         os.remove(filename.format(i))
     except:
         pass
-
+   
 sheet = 'Sheet{0}'
 filename_x = 'test_{0}.xlsx'
 
+#set-up writer
 book = load_workbook('Book1.xlsx')
 writer = pd.ExcelWriter('Book1.xlsx', engine='openpyxl')
 writer.book = book
 
+#separate according to financial statement type
 for i in asx['ASX Code']:
     file = filename_x.format(i)
     try:
